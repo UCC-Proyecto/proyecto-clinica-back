2040dfbf4d630c674580836d68b2746d1991d25b
adrianaparedess <adriana.paredess@campusucc.edu.co>
2026-07-02 09:26:59 -0500

    feat: create doctor entity

---
+++ src/main/java/com/ucc/clinica/entity/Medico.java
@@ -0,0 +1,20 @@
+package com.ucc.clinica.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="medicos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+}
