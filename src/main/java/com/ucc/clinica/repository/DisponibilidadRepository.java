6021608ca23d1355c3ea699192741f5936aafa42
adrianaparedess <adriana.paredess@campusucc.edu.co>
2026-07-02 09:37:00 -0500

    feat: create availibility repository

---
+++ src/main/java/com/ucc/clinica/repository/DisponibilidadRepository.java
@@ -0,0 +1,9 @@
+package com.ucc.clinica.repository;
+
+import com.ucc.clinica.entity.Disponibilidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DisponibilidadRepository extends JpaRepository<Disponibilidad, Long> {
+}
