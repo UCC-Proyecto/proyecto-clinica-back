0c59d2e800b0c20eca0e7d724506cf25a81bd74b
adrianaparedess <adriana.paredess@campusucc.edu.co>
2026-07-02 09:35:48 -0500

    feat: create availability entity

---
+++ src/main/java/com/ucc/clinica/entity/Disponibilidad.java
@@ -0,0 +1,28 @@
+package com.ucc.clinica.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "dispobilidades")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Disponibilidad {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate fecha;
+    private LocalTime hora;
+    @Enumerated(EnumType.STRING)
+    private EstadoDisponibilidad estado;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+}
