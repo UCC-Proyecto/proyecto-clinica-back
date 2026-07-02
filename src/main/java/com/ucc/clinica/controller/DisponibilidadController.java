650cec26496d0c1934d9a8cacc0d6dc8682cda87
adrianaparedess <adriana.paredess@campusucc.edu.co>
2026-07-02 11:12:06 -0500

    feat: add availability registration endpoint

---
+++ src/main/java/com/ucc/clinica/controller/DisponibilidadController.java
@@ -0,0 +1,26 @@
+package com.ucc.clinica.controller;
+
+import com.ucc.clinica.dto.request.DisponibilidadRequest;
+import com.ucc.clinica.dto.response.DisponibilidadResponse;
+import com.ucc.clinica.entity.Disponibilidad;
+import com.ucc.clinica.service.DisponibilidadService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/disponibilidades")
+@RequiredArgsConstructor
+public class DisponibilidadController {
+    private final DisponibilidadService disponibilidadService;
+
+    @PostMapping
+    public ResponseEntity<DisponibilidadResponse> registrar(@Valid @RequestBody DisponibilidadRequest disponibilidadrequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(disponibilidadService.registrar(disponibilidadrequest));
+    }
+}
