c8b5fee329e3b0de39ee93c824a94544198d10c6
adrianaparedess <adriana.paredess@campusucc.edu.co>
2026-07-03 10:33:17 -0500

    feat: create appointment history dto

---
+++ src/main/java/com/ucc/clinica/dto/response/HistorialCitaResponse.java
@@ -0,0 +1,19 @@
+package com.ucc.clinica.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Builder
+public class HistorialCitaResponse {
+    private Long id;
+    private String medico;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private String estado;
+}
