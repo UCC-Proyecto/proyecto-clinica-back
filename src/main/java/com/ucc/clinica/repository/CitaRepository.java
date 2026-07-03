8a32376543f5773c5e5864dc9615acc989f0b3f8
adrianaparedess <adriana.paredess@campusucc.edu.co>
2026-07-03 10:31:16 -0500

    feat: add patient appontment query

--- src/main/java/com/ucc/clinica/repository/CitaRepository.java
+++ src/main/java/com/ucc/clinica/repository/CitaRepository.java
@@ -4,6 +4,9 @@ import com.ucc.clinica.entity.Cita;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CitaRepository extends CrudRepository<Cita, Long> {
+    List<Cita> findByPacienteId(Long pacienteId);
 }
