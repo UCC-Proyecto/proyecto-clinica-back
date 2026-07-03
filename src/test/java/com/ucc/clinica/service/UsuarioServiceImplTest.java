22c67fd061c74667991d1185713d14f71a41c2e5
adrianaparedess <adriana.paredess@campusucc.edu.co>
2026-07-03 11:33:39 -0500

    test: configure H2 database for automated test

--- src/test/java/com/ucc/clinica/service/UsuarioServiceImplTest.java
+++ src/test/java/com/ucc/clinica/service/UsuarioServiceImplTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
